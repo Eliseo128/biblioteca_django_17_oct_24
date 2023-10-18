8edbb80f5b09cd1bde290cad6a8ec0380086bcd0
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2023-10-18 14:16:43 +0200

    creado libro.html y añadido el metodo devolver_libro en views

--- Biblioteca/urls.py
+++ Biblioteca/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns =[
     path('',views.index,name='index'),#en vista.py coge la funcion index y la ejecuta
     path('libros/lista',views.listar_libros,name='lista_libros'),
+    path('libros/<int:id_libro>/',views.devolver_libro,name='devolver_libro'),
 ]
--- Biblioteca/views.py
+++ Biblioteca/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from models import *
+from .models import Libro
+from .models import Biblioteca
+from .models import Autor
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def listar_libros(request):
-    libros= Libro.objects.select_related('biblioteca').prefetch_related('autores')#variable libros = clase libro y su relacion con biblioteca y autores
+    libros= Libro.objects.all()#esta opcion funciona igual que la de select_related y prefetch_related pero es menos optima
+    libros=Libro.objects.select_related('biblioteca').prefetch_related('autores')
+    # #variable libros = clase libro y su relacion con biblioteca y autores
     #se usa select_related con relaciones 1-n y prefetch_related con relaciones n-m
     #Libro.objects es un queryset que obtiene todos los datos de los objetos Libro(como usar Libro.objects.all(()))
     return render(request,'libro/lista.html',{'libros_mostrar':libros})
@@ -14,3 +18,6 @@ def listar_libros(request):
 #libro/lista.html es el archivo que está en templates/libro/lista
 #{libros_mostrar:libros} es un diccionario, la key sirve para listar los elementos de la clase libro y acceder a sus values
 
+def devolver_libro(request,id_libro):
+    libro=Libro.objects.select_related('biblioteca').prefetch_related('autores').get(id=id_libro)
+    return render(request,'libro/libro.html',{'libro_mostrar':libro})
