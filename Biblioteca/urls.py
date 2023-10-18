3de96563b6c2a1393b4010ecd1d1f54b0b395615
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2023-10-18 23:00:08 +0200

    añadidos los directorios biblioteca y cliente a templates junto a sus html, ambos funcionan

--- Biblioteca/urls.py
+++ Biblioteca/urls.py
@@ -5,4 +5,6 @@ urlpatterns =[
     path('',views.index,name='index'),#en vista.py coge la funcion index y la ejecuta
     path('libros/lista',views.listar_libros,name='lista_libros'),
     path('libros/<int:id_libro>/',views.devolver_libro,name='devolver_libro'),
+    path('biblioteca/biblioteca',views.datos_biblio,name='datos_biblio'),#el primer elemento de la tupla es el que hay que poner en la url para que cargue
+    path('cliente/cliente',views.clientes_registrados,name='clientes_registrados')#el segundo elemento es el nombre de la funcion en el archivo views.py
 ]
--- Biblioteca/views.py
+++ Biblioteca/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Libro
 from .models import Biblioteca
-from .models import Autor
+from .models import DatosCliente #lo hago con esta clase porque al tener una FK de cliente puedo acceder a los datos de ambas
 
 # Create your views here.
 def index(request):
@@ -11,7 +11,7 @@ def listar_libros(request):
     libros= Libro.objects.all()#esta opcion funciona igual que la de select_related y prefetch_related pero es menos optima
     libros=Libro.objects.select_related('biblioteca').prefetch_related('autores')
     # #variable libros = clase libro y su relacion con biblioteca y autores
-    #se usa select_related con relaciones 1-n y prefetch_related con relaciones n-m
+    #se usa select_related con relaciones 1-n o 1-1 y prefetch_related con relaciones n-m
     #Libro.objects es un queryset que obtiene todos los datos de los objetos Libro(como usar Libro.objects.all(()))
     return render(request,'libro/lista.html',{'libros_mostrar':libros})
 #request hace referencia a la solicitud que se recibe
@@ -21,3 +21,11 @@ def listar_libros(request):
 def devolver_libro(request,id_libro):
     libro=Libro.objects.select_related('biblioteca').prefetch_related('autores').get(id=id_libro)
     return render(request,'libro/libro.html',{'libro_mostrar':libro})
+
+def datos_biblio(request):
+    biblio=Biblioteca.objects.all()
+    return render(request,'biblioteca/biblioteca.html',{'datos':biblio})#el nombre del value tiene que ser = al de la variable que accede a los objetos
+
+def clientes_registrados(request):
+    datos_cl=DatosCliente.objects.select_related('cliente')
+    return render(request,'cliente/cliente.html',{'cliente_datos':datos_cl})
